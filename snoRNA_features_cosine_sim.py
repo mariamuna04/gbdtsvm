33f95a37142ca8883e177f150565dba7718cbbdd
FahimHafiz <fahimeee2015@gmail.com>
2025-01-04 00:01:18 +0600

    code update

--- snoRNA_features_cosine_sim.py
+++ snoRNA_features_cosine_sim.py
@@ -1,8 +1,16 @@
+import os
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics.pairwise import cosine_similarity
 
-df = pd.read_csv('MDRF_Data/snoRNA_features.csv')
+# Define input and output directories
+input_data_dir = "data"
+output_dir = "results"
+# Ensure the output directory exists
+os.makedirs(output_dir, exist_ok=True)
+
+df= pd.read_csv(os.path.join(input_data_dir, 'snoRNA_features.csv'))
+
 
 # the first column contains IDs or, thats why we ignored it.
 feature_matrix = df.iloc[:, 1:].values
@@ -15,5 +23,7 @@ F_normalized = scaler.fit_transform(feature_matrix)
 similarity_matrix = cosine_similarity(F_normalized)
 
 # Step 4: Save the similarity matrix to a CSV file
-output_file = 'MDRF_output/snoRNA_similarity.csv'
+output_file = os.path.join(output_dir, "snoRNA_similarity.csv")
 pd.DataFrame(similarity_matrix).to_csv(output_file, index=False)
+
+print(f"Similarity matrix successfully saved to: {output_file}")
