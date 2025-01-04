7bff43f54d2b86e7eb83c95eff84925580b37032
FahimHafiz <fahimeee2015@gmail.com>
2025-01-05 00:19:56 +0600

    Update data_format.py

--- examples/PSnoD-GBDTSVM/data_format.py
+++ examples/PSnoD-GBDTSVM/data_format.py
@@ -2,9 +2,10 @@ import os
 import numpy as np
 import pandas as pd
 
-sage_disease_sim = pd.read_csv(r'SAGESDA_Data\disease_sim_graph_filtered.csv')
-sage_rna_sim = pd.read_csv(r'SAGESDA_Data\snoRNA_4mer_similarity.csv')
-sage_dis_rna_relation = pd.read_csv(r'SAGESDA_Data\relationship_matrix_filtered.csv')
+# Set the directory for the PSnoD datasets
+sage_disease_sim = pd.read_csv(r'raw_data\disease_sim_graph_filtered.csv')
+sage_rna_sim = pd.read_csv(r'raw_data\snoRNA_4mer_similarity.csv')
+sage_dis_rna_relation = pd.read_csv(r'raw_data\relationship_matrix_filtered.csv')
 
 disease_name = sage_disease_sim.iloc[:, 0]
 snoRNA_name = sage_rna_sim.iloc[:, 0]
