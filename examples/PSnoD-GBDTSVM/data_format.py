45fcb8e3b85c8549e8ae716631478b6bd2e29671
FahimHafiz <fahimeee2015@gmail.com>
2025-01-05 00:32:32 +0600

    Update data_format.py

--- examples/PSnoD-GBDTSVM/data_format.py
+++ examples/PSnoD-GBDTSVM/data_format.py
@@ -3,12 +3,21 @@ import numpy as np
 import pandas as pd
 
 # Set the directory for the PSnoD datasets
-sage_disease_sim = pd.read_csv(r'raw_data\disease_sim_graph_filtered.csv')
-sage_rna_sim = pd.read_csv(r'raw_data\snoRNA_4mer_similarity.csv')
-sage_dis_rna_relation = pd.read_csv(r'raw_data\relationship_matrix_filtered.csv')
+disease_sim = pd.read_csv(r'raw_data\disease_sim_graph_filtered.csv')
+rna_sim = pd.read_csv(r'raw_data\snoRNA_4mer_similarity.csv')
+disease_rna_relation = pd.read_csv(r'raw_data\relationship_matrix_filtered.csv')
 
-disease_name = sage_disease_sim.iloc[:, 0]
-snoRNA_name = sage_rna_sim.iloc[:, 0]
-SnoRNA_similarity = pd.read_csv('SAGESDA_output/IRS_matrix.csv', header=None)
-known_association = sage_dis_rna_relation.iloc[:, 1:].T
-disease_similarity = pd.read_csv('SAGESDA_output/IDS_matrix.csv', header=None)
+# output formatted data directory
+
+output_dir = 'data'
+
+disease_name = disease_sim.iloc[:, 0]
+snoRNA_name = rna_sim.iloc[:, 0]
+known_association = disease_rna_relation.iloc[:, 1:].T
+disease_similarity = disease_sim.iloc[:, 1:]
+SnoRNA_similarity = rna_sim.iloc[:, 1:]
+
+
+# save to csv
+disease_similarity_csv_path = os.path.join(output_dir, "disease_similarity.csv")
+disease_similarity.to_csv(disease_similarity_csv_path, index=False, header=None)
