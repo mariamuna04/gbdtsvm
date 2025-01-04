7108d6f2a0fc69cc9a363cefc0ba5c131acfb2a2
FahimHafiz <fahimeee2015@gmail.com>
2025-01-05 00:10:04 +0600

    Update data_format.py

--- examples/PSnoD-GBDTSVM/data_format.py
+++ examples/PSnoD-GBDTSVM/data_format.py
@@ -1,21 +1,6 @@
-import random
+import os
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-from numpy import interp
-from sklearn.cluster import KMeans
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.preprocessing import OneHotEncoder
-from sklearn import svm
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import GridSearchCV
-from sklearn.pipeline import Pipeline
-
-from sklearn.metrics import precision_recall_curve, roc_curve, auc, confusion_matrix, accuracy_score, classification_report
-from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import StratifiedKFold
-
-from sklearn.metrics import f1_score
 
 sage_disease_sim = pd.read_csv(r'SAGESDA_Data\disease_sim_graph_filtered.csv')
 sage_rna_sim = pd.read_csv(r'SAGESDA_Data\snoRNA_4mer_similarity.csv')
